0086ba9795a52443ec66b794de70be1c5201f70c
vishal agarwal <vishal.agarwal@niveussolutions.com>
2024-01-15 21:00:57 +0530

    Connect to mongo database

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.CONNECTION_URL}/${DB_NAME}`)
+        console.log(`\n MongoDb connection !! DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch (error) {
+        console.log("MongoDB connection failed: " + error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
