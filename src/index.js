d075777eb3a75083b487917663100e4ff30d2c06
vishal agarwal <vishal.agarwal@niveussolutions.com>
2024-01-15 21:46:00 +0530

    Util code changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import express from "express";
+const app = express();
 dotenv.config({
     path: './.env',
 });
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+        app.on("error", () => {
+            console.log("Error", error);
+            throw error;
+        })
+    }).catch((err) => {
+        console.log("MongoDb connect failed", err);
+    })
 
 
 
